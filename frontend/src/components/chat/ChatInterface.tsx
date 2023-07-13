33e24b3b6b67ff66f8f227819c7ca470a5e14aae
ashishjhaO <66025928+ashishjhaO@users.noreply.github.com>
2023-07-13 15:55:44 +0530

    Update ChatInterface.tsx

--- frontend/src/components/chat/ChatInterface.tsx
+++ frontend/src/components/chat/ChatInterface.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Avatar, Button, Box, Grid, Typography } from '@mui/material';
 import { deepOrange, deepPurple } from '@mui/material/colors';
-import { getAnswer } from '../../apis/api';
+import { getAnswer, summarizeDocument } from '../../apis/api';
+import { AppBar, TextField } from '@mui/material';
+
 interface ChatMessage {
   id: string;
   message: string;
@@ -9,104 +11,7 @@ interface ChatMessage {
   avatar: string;
 }
 
-const ChatInterface: React.FC = () => {
-  const [messages, setMessages] = useState<ChatMessage[]>([]);
-  const [inputMessage, setInputMessage] = useState('');
-  const chatMessageRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const chatMessageLog = chatMessageRef.current;
-    if (chatMessageLog) {
-      chatMessageLog.scrollTop = chatMessageLog.scrollHeight;
-    }
-  }, [messages]);
-
-  const handleInputMessageChange = (
-    event: React.ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    setInputMessage(event.target.value);
-  };
-
-  const handleKeyDown = (event: React.KeyboardEvent) => {
-    if (event.key === 'Enter') {
-      event.preventDefault(); // Prevents a new line from being added
-      handleInputMessageSubmit();
-    }
-  };
-
-  const handleInputMessageSubmit = async () => {
-    if (inputMessage.trim() === '') {
-      return;
-    }
-
-    const newMessage: ChatMessage = {
-      id: Math.random().toString(36).substring(7),
-      message: inputMessage,
-      isUser: true,
-      avatar: 'https://example.com/user-avatar.png',
-    };
-
-    setMessages((messages) => [
-      ...messages,
-      newMessage,
-      {
-        id: Math.random().toString(36).substring(7),
-        message: 'Loading...',
-        isUser: false,
-        avatar: 'https://example.com/chatbot-avatar.png',
-      },
-    ]);
-
-    setInputMessage('');
-
-    // Here you can send the inputMessage to the server to get the response
-    // Once the response is received, you can update the messages array with the response
-    const result = await getAnswer(inputMessage);
-    // const result = { response: Math.random().toString() };
-    // console.log('result', result);
-
-    const chatbotMessage: ChatMessage = {
-      id: Math.random().toString(36).substring(7),
-      message: result.response,
-      isUser: false,
-      avatar: 'https://example.com/chatbot-avatar.png',
-    };
-
-    setMessages((prevMessages) => [
-      ...prevMessages.slice(0, -1),
-      chatbotMessage,
-    ]);
-  };
-
-  return (
-    <div ref={chatMessageRef} className="chatbox">
-      <div className="chat-logs">
-        {messages.map((message, index) => (
-          <Message key={index} message={message} />
-        ))}
-      </div>
-      <div className="chat-input-holder">
-        <textarea
-          value={inputMessage}
-          className="chat-input"
-          rows={1}
-          onKeyDown={handleKeyDown}
-          onChange={handleInputMessageChange}
-        />
-
-        <button className="chat-input-btn" onClick={handleInputMessageSubmit}>
-          <i
-            className="fa fa-paper-plane chat-input-send-icon"
-            aria-hidden="true"
-          ></i>
-          Send
-        </button>
-      </div>
-    </div>
-  );
-};
-
-export interface MessageProps {
+interface MessageProps {
   id: string;
   message: string;
   isUser: boolean;
@@ -137,6 +42,169 @@ const Message = ({ message }: { message: MessageProps }) => {
       </Typography>
     </div>
   );
+};
+
+const ChatInterface: React.FC = () => {
+  const [messages, setMessages] = useState<ChatMessage[]>([]);
+  const [inputMessage, setInputMessage] = useState('');
+  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+  const [selectedFileForSummarization, setSelectedFileForSummarization] = useState<string | null>(null);
+  const chatMessageRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const chatMessageLog = chatMessageRef.current;
+    if (chatMessageLog) {
+      chatMessageLog.scrollTop = chatMessageLog.scrollHeight;
+    }
+  }, [messages]);
+
+  const handleInputMessageChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setInputMessage(event.target.value);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      handleInputMessageSubmit();
+    }
+  };
+
+  const handleInputMessageSubmit = async () => {
+    if (inputMessage.trim() === '') {
+      return;
+    }
+
+    const newMessage: ChatMessage = {
+      id: Math.random().toString(36).substring(7),
+      message: inputMessage,
+      isUser: true,
+      avatar: 'https://example.com/user-avatar.png',
+    };
+
+    setMessages((messages) => [
+      ...messages,
+      newMessage,
+      {
+        id: Math.random().toString(36).substring(7),
+        message: 'Loading...',
+        isUser: false,
+        avatar: 'https://example.com/chatbot-avatar.png',
+      },
+    ]);
+
+    setInputMessage('');
+
+    const result = await getAnswer(inputMessage);
+
+    const chatbotMessage: ChatMessage = {
+      id: Math.random().toString(36).substring(7),
+      message: result.response,
+      isUser: false,
+      avatar: 'https://example.com/chatbot-avatar.png',
+    };
+
+    setMessages((prevMessages) => [
+      ...prevMessages.slice(0, -1),
+      chatbotMessage,
+    ]);
+  };
+
+  const handleSummarize = async () => {
+    // if (selectedFileForSummarization === null) {
+    //   return;
+    // }
+  
+    const newMessage: ChatMessage = {
+      id: Math.random().toString(36).substring(7),
+      message: 'Summarizing document...',
+      isUser: true,
+      avatar: 'https://example.com/user-avatar.png',
+    };
+  
+    setMessages((messages) => [
+      ...messages,
+      newMessage,
+      {
+        id: Math.random().toString(36).substring(7),
+        message: 'Loading...',
+        isUser: false,
+        avatar: 'https://example.com/chatbot-avatar.png',
+      },
+    ]);
+  
+    const result = await summarizeDocument(selectedFileForSummarization);
+  
+    const summaryMessage: ChatMessage = {
+      id: Math.random().toString(36).substring(7),
+      message: result.response,
+      isUser: false,
+      avatar: 'https://example.com/chatbot-avatar.png',
+    };
+  
+    setMessages((prevMessages) => [
+      ...prevMessages.slice(0, -1),
+      summaryMessage,
+    ]);
+  };
+  
+  
+  
+
+
+  return (
+    <div className="chatbox">
+      <div className="chat-logs" ref={chatMessageRef}>
+        {messages.map((message, index) => (
+          <Message key={index} message={message} />
+        ))}
+      </div>
+      <AppBar position="fixed" color="primary" style={{ top: 'auto', bottom: 0 }}>
+        <Box display="flex" alignItems="center" p={1}>
+          <TextField
+            value={inputMessage}
+            className="chat-input"
+            fullWidth
+            variant="outlined"
+            placeholder="Type your message..."
+            onKeyDown={handleKeyDown}
+            onChange={handleInputMessageChange}
+          />
+  
+          <Button
+            className="chat-input-btn"
+            variant="contained"
+            color="secondary"
+            onClick={handleInputMessageSubmit}
+            style={{ marginLeft: '10px' }}
+          >
+            <i
+              className="fa fa-paper-plane chat-input-send-icon"
+              aria-hidden="true"
+            ></i>
+            Send
+          </Button>
+  
+          <Button
+            className="chat-input-btn"
+            variant="contained"
+            color="secondary"
+            onClick={handleSummarize}
+            style={{ marginLeft: '10px' }}
+          >
+            <i
+              className="fa fa-book chat-input-send-icon"
+              aria-hidden="true"
+            ></i>
+            Summarize
+          </Button>
+        </Box>
+      </AppBar>
+    </div>
+  );
+  
+  
 };
 
 export default ChatInterface;
