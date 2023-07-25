af501362ecbf8ba2cb50e253a1b0a7e1b279b0a8
Nishant Sinha <nishantsinha00@gmail.com>
2023-07-25 16:55:13 +0530

    requirements.txt updated and summaries integrated with postgres

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,8 +38,18 @@ from dotenv import load_dotenv
 
 import os
 
+import psycopg2
+
 load_dotenv()
+
+DATABASE_HOST = "localhost"
+DATABASE_PORT = 5432
+DATABASE_USER = "postgres"
+DATABASE_PASSWORD = "115211"
+DATABASE_NAME = "postgres"
+
 index_name = "langchain-demo"
+
 pinecone.init(api_key=os.environ["PINECONE_API_KEY"], 
               environment=os.environ["PINECONE_ENV"])
 if index_name not in pinecone.list_indexes():
@@ -51,8 +61,26 @@ if index_name not in pinecone.list_indexes():
     )
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
-docs = []
+# docs = []
 # from sentence_transformers import SentenceTransformer
+
+document_types = [
+    "Lab reports",
+    "Imaging reports",
+    "Genetic reports",
+    "Histopathology reports",
+    "Prescriptions",
+    "Pill bottle photos",
+    "Clinician notes",
+    "Discharge notes",
+    "End of Visit Summaries",
+    "Healthcare bills",
+    "Legal documents",
+    "Insurance documents",
+    "Advance care planning",
+    "Financial documents",
+    "Miscellaneous"
+]
 
 
 app = FastAPI(
@@ -103,6 +131,7 @@ class Reader:
         extracted_text = "\n".join(lines)
 
         return extracted_text
+    
 def img_to_doc(img_file):
     docs = []
     reader = Reader(img=img_file)
@@ -113,8 +142,7 @@ def img_to_doc(img_file):
         doc.metadata['date']= datetime.date.today().strftime('%Y-%m-%d')
         
     return docs
-    
-        
+          
 def pdf_to_doc(pdf_file):
     i = 0
     loader = PyPDFLoader(pdf_file)
@@ -152,27 +180,96 @@ def get_qa_chain(namespace = None, dateRange = None):
     ) 
     return qa
 
+def get_summary(docs):
+    llm = OpenAI(temperature=0)
+    chain = load_summarize_chain(llm, 
+                             chain_type="map_reduce",
+                             verbose = True)
+    output_summary = chain.run(docs)
+    wrapped_text = textwrap.fill(output_summary, width=100)
+    return wrapped_text
+
+def create_file_summaries_table(conn):
+    try:
+        cursor = conn.cursor()
+        create_table_query = '''
+            CREATE TABLE IF NOT EXISTS file_summaries (
+                id SERIAL PRIMARY KEY,
+                filename TEXT NOT NULL,
+                file_type TEXT NOT NULL,
+                summary TEXT NOT NULL
+            )
+        '''
+        cursor.execute(create_table_query)
+        conn.commit()
+        cursor.close()
+    except Exception as ex:
+        print("Error creating file_summaries table:", str(ex))
+
+def save_file_summary(filename, file_type, summary, conn):
+    try:
+        cursor = conn.cursor()
+        insert_query = "INSERT INTO file_summaries (filename, file_type, summary) VALUES (%s, %s, %s)"
+        cursor.execute(insert_query, (filename, file_type, summary))
+        conn.commit()
+        cursor.close()
+    except Exception as ex:
+        print("Error saving file summary:", str(ex))
+
+def fetch_file_summary_by_filename(filename, conn):
+    try:
+        cursor = conn.cursor()
+        select_query = "SELECT summary FROM file_summaries WHERE filename = %s"
+        cursor.execute(select_query, (filename,))
+        file_summary = cursor.fetchone()
+        conn.commit()
+        cursor.close()
+        if file_summary:
+            return file_summary
+        else:
+            raise HTTPException(status_code=404, detail="File summary not found.")
+    except HTTPException:
+        raise
+    except Exception as ex:
+        print("Error fetching file summary:", str(ex))
+        raise HTTPException(status_code=500, detail="Error fetching file summary.")
+
 class UserQuery(BaseModel):
     query: str
 
-
-
+class DateRange(BaseModel):
+    dateRange: Optional[List] = None
+    
+    class Config:
+        schema_extra = {
+            "example": {
+            }
+        }
 
 @app.get("/")
 async def root(request: Request):
     return {"message": "Server is up and running!"}
 
 
-
 @app.post("/upload-files")
 async def upload_file(request: Request, 
                       files: List[UploadFile],
-                      file_type: str = Query("Lab Report", enum=["Lab Report", "Prescription", "Other"]),
+                      file_type: str = Query(document_types[0], enum=document_types),
                       ):
-    
+    conn = psycopg2.connect(
+        host=DATABASE_HOST,
+        port=DATABASE_PORT,
+        user=DATABASE_USER,
+        password=DATABASE_PASSWORD,
+        dbname=DATABASE_NAME
+    )
+    docs = []
+
+    create_file_summaries_table(conn)
+
     for file in files:
         filename = file.filename
-        status = "success"
+        summary = "success"
         print(file.size)
         try:
             if not os.path.exists('app/documents'):
@@ -185,46 +282,55 @@ async def upload_file(request: Request,
                 docs.extend(pdf_to_doc(filepath))
             else:
                 docs.extend(img_to_doc(filepath))
+            summary = get_summary(docs=docs)
+            save_file_summary(filename, file_type, summary, conn)
         except Exception as ex:
             print(str(ex))
-            status = "error"
+            summary = "error"
             if filepath is not None and os.path.exists(filepath):
                 os.remove(filepath)
-            # raise HTTPException(status_code=500, detail="Your file received but couldn't be stored!")
+            # raise HTTPException(summary_code=500, detail="Your file received but couldn't be stored!")
     
         if filepath is not None and os.path.exists(filepath):
             os.remove(filepath)
+    conn.close()
     docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name, namespace=file_type)
-    return {"filename": filename, "status": status}
-
-class DateRange(BaseModel):
-    dateRange: Optional[List] = None
-    
-    class Config:
-        schema_extra = {
-            "example": {
-            }
-        }
-
-@app.post("/")
-
-
+    return {"filename": filename, "summary": summary}
 
 @app.post("/query")
 async def query_index(request: Request, 
                       input_query: UserQuery, 
                       dateRange: DateRange,
-                      namespace: str = Query("Lab Report", enum=["Lab Report", "Prescription", "Other"]),
+                      namespace: str = Query(document_types[0], enum=document_types),
                       ):
     dateRange = dateRange.dateRange
-    print(dateRange)
     qa_chain = get_qa_chain(namespace = namespace, dateRange = dateRange)
     result = qa_chain({"question": input_query.query})
-    print(result)
     return {"response": result['answer'], "relevant_docs": result['source_documents']}
 
+@app.post("/file-summary")
+def get_file_summary_by_filename(filename: str):
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(
+        host=DATABASE_HOST,
+        port=DATABASE_PORT,
+        user=DATABASE_USER,
+        password=DATABASE_PASSWORD,
+        database=DATABASE_NAME
+    )
 
-@app.post("/summarize")
+    try:
+        # Fetch file summary from the database based on the filename
+        file_summary = fetch_file_summary_by_filename(filename, conn)
+        return file_summary
+    except HTTPException as http_ex:
+        # Propagate the HTTPException if raised in the fetch_file_summary_by_filename function
+        raise http_ex
+    finally:
+        # Close the database connection
+        conn.close()
+
+"""@app.post("/summarize")
 async def summarize(request: Request):
     llm = OpenAI(temperature=0)
     chain = load_summarize_chain(llm, 
@@ -232,7 +338,8 @@ async def summarize(request: Request):
                              verbose = True)
     output_summary = chain.run(docs)
     wrapped_text = textwrap.fill(output_summary, width=100)
-    return {"response": wrapped_text}
+    return {"response": wrapped_text}"""
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8000)
